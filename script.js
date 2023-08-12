e23fd8741edcaf124268f92eebec403c845f298b
phogatheena <109434462+phogatheena@users.noreply.github.com>
2023-08-13 00:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
- // Create an object to store character frequencies
+function firstNonRepeatedChar(s) {
     const charCount = {};
-
-    // Count the frequency of each character in the string
-    for (const char of str) {
-        charCount[char] = (charCount[char] || 0) + 1;
+    
+    for (const char of s) {
+        if (char in charCount) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
     }
-
-    // Find the first non-repeated character
-    for (const char of str) {
+    
+    for (const char of s) {
         if (charCount[char] === 1) {
             return char;
         }
     }
-
-    // If no non-repeated character is found, return null
+    
     return null;
 }
